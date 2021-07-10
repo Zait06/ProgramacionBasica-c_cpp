e327cad4ce78f71668dd3eace33136dc32c87714
Zait06 <zait-hdez@hotmail.com>
2021-07-09 22:19:37 -0500

    Apuntadores

---
+++ prog09_apuntadores/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	// Declaracion
+	int *ptr;
+    int a = 80;
+    
+    // Asignacion
+    ptr = &a;
+    
+    // Impresion de datos
+    printf("Dirección del apuntador %p\n", ptr);
+    printf("Valor que guarda el apuntador %d\n", *ptr);
+	
+	return 0;
+}
